408ca5dbe591016658906af9242b6b57639c2c83
AbhishaAbhi <abhiabhisha2003@gmail.com>
2025-09-22 12:17:01 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,21 @@
+#adding item to the list
+Fruit=['Apple','Orange','Banana','Grapes']
+print(Fruit)
+print(Fruit[1])
+print("newly list")
+Fruit.append('Water Melon')
+print(Fruit)
+Fruit.insert(2,'Guava')
+print(Fruit)
+fr=('Kivi','Jackfruit','Pine apple')
+Fruit.extend(fr)
+print(Fruit)
+f1='pomegranate'
+Fruit.extend(f1)
+print(Fruit)
+
+vegetable=('onion','tomato')
+print(vegetable)
+print(vegetable[1])
+
+
