93c45497209711c6d677bb5918a536ff5798a74e
AbhishaAbhi <abhiabhisha2003@gmail.com>
2025-11-27 01:24:26 -0800

    Add files via upload

---
+++ CO5_5FUNCTION.py
@@ -0,0 +1,28 @@
+import csv
+#input dictionary using user input method
+data={}
+n=int(input("Enter the limit:"))
+for i in range(n):
+ key=input("Enter the key:")
+ val=input("Enter the value:")
+ data[key]=val
+print(data)
+def cwrite():
+    
+#csv writing
+
+    f=open("data.csv","w",newline='')
+    content=csv.writer(f)
+    content.writerow(data.keys())
+    content.writerow(data.values())
+    f.close()
+def cread():
+#csv reading
+
+    f1=open("data.csv","r")
+    c=csv.reader(f1)
+    for i in c:
+     print(i)
+    f1.close()
+cwrite()
+cread()
