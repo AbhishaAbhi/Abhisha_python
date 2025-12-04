262e6385ba9da311ab2c8e650a9b40d61bc0c7fa
AbhishaAbhi <abhiabhisha2003@gmail.com>
2025-12-04 07:37:19 -0800

    Add files via upload

---
+++ prgm4.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the list:"))
+list=[]
+sum = 0
+for i in range(n):
+    elements = int(input("Enter the elements:"))
+    list.append(elements)
+    sum=elements + i
+    print(list)
+    print(sum)
+    
+                 
