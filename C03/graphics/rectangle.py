c6840181f895e81f4c438db6e53f78ce7c00608f
AbhishaAbhi <abhiabhisha2003@gmail.com>
2025-12-08 02:31:50 -0800

    Add files via upload

---
+++ C03/graphics/rectangle.py
@@ -0,0 +1,7 @@
+#Create a package with modules rectangle , circle ,and subpackage (3D graphics) with modules cuboid and sphere. Include methods to find area and perimeter of respective figures un each module. Write a main program to find area and perimeter by importing statement.
+
+
+def area(length,breadth):
+    return length * breadth
+def perimeter(length,breadth):
+    return 2 * (length+breadth)
